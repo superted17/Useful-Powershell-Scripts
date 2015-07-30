92aef95147ada9238afd96bf00a2161af73a293e
Ed Spencer <efjspencer@gmail.com>
2015-07-30 10:54:00 +0100

    Cleaning up alignment

--- Services/stop-service-if-running.ps1
+++ Services/stop-service-if-running.ps1
@@ -5,14 +5,16 @@
 
 function CheckService{
 	param($ServiceName)
+	
 	$arrService = Get-Service -Name $ServiceName
+	
 	if ($arrService.Status -eq "Running"){
-	Stop-Service $ServiceName
-	Write-Host "Stopping " $ServiceName " service" 
-
+		Stop-Service $ServiceName
+		Write-Host "Stopping " $ServiceName " service" 
 	}
+	
 	if ($arrService.Status -ne "running"){ 
-	Write-Host "$ServiceName service is already stopped"
+		Write-Host "$ServiceName service is already stopped"
 	}
  }
  
